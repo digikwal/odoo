622031def2c2afb6d29b7b49e31a0e55cf7e4f6e
Digikwal <79085106+digikwal@users.noreply.github.com>
2026-04-18 04:18:31 +0200

    Create __manifest__.py

---
+++ extra-addons/web_pwa_start_url_config/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# See LICENSE file for full copyright and licensing details.
+{
+    'name': 'PWA Start URL Config',
+    'summary': 'Configure PWA start_url via odoo.conf',
+    'version': '18.0.1.0.0',
+    'category': 'Web',
+    'author': 'Digikwal',
+    'depends': ['web'],
+    'data': [],
+    'installable': True,
+    'license': 'LGPL-3',
+}
